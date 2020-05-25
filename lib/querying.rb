19a261287612a967172ec2e3356f1770313e11c1
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-25 03:53:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,10 +31,15 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title 
-  FROM series 
-  JOIN characters ON series.id = characters.series_id 
-  WHERE characters.species = 'human' LIMIT 1"
+ "SELECT series.title
+  FROM series
+  INNER JOIN books ON series.id = books.series_id
+  INNER JOIN character_books ON books.id = character_books.character_id
+  INNER JOIN characters ON character_books.character_id = characters.id
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species) DESC
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
